8ce0c9e4d70bd869ff40163f2a802b1300d11bf3
Aminaahmed00 <aminaahmed20001@gmail.com>
2025-03-26 23:22:41 +0200

    test commit

--- src/test/java/openBrowsers/OpenChrome.java
+++ src/test/java/openBrowsers/OpenChrome.java
@@ -1,22 +1,29 @@
 package openBrowsers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenChrome {
-    public static void main(String[] str){
+    public static void main(String[] str) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://the-internet.herokuapp.com/login");
         driver.manage().window().maximize();
+//        invalid login
 
-        Dimension iphoneXR = new Dimension(414, 896);
-        driver.manage().window().setSize(iphoneXR);
-        driver.navigate().to("https://www.youtube.com");
-        driver.navigate().back();
-        driver.navigate().forward();
-        // test
+        driver.findElement(By.id("username")).sendKeys("Ahmed");
+        driver.findElement(By.id("password")).sendKeys("Ajhu@1458#");
+        driver.findElement(By.className("radius")).click();
+
+        driver.findElement(By.id("username")).sendKeys("Ahmed");
+        driver.findElement(By.id("password")).sendKeys("####");
+        driver.findElement(By.className("radius")).click();
+
+        driver.findElement(By.id("username")).sendKeys("Ahmed");
+        driver.findElement(By.id("password")).sendKeys("1458");
+        driver.findElement(By.className("radius")).click();
     }
 }
